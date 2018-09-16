382d96102f6d6f14a657da8ea0a5d7771e85b64e
Cleveland PyLadies <39771542+CLEPyLadies@users.noreply.github.com>
2018-09-16 10:12:44 -0400

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -21,7 +21,7 @@ env.cloudfiles_api_key = 'my_rackspace_api_key'
 env.cloudfiles_container = 'my_cloudfiles_container'
 
 # Github Pages configuration
-env.github_pages_branch = "master"
+env.github_pages_branch = "gh-pages"
 
 # Port for `serve`
 PORT = 8000

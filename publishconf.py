9dcee5e4a6ee08268b7e556942896b865887b957
mutterberg <Marissa@UtterbergDataDev.com>
2018-09-16 10:08:20 -0400

    Fixed url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://clepyladies.github.io'
+SITEURL = 'https://clepyladies.github.io/home'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'

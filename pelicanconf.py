1c88f0ecc8d6e971c7463e130b9a329a4ab96344
mutterberg <Marissa@UtterbergDataDev.com>
2018-10-09 15:01:35 -0400

    Change LOCALE to en_US.UTF-8

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,7 +34,8 @@ MAIN_MENU = True
 PATH = 'content'
 
 TIMEZONE = 'America/New_York'
-OG_LOCALE = 'en_US.UTF-8'
+OG_LOCALE = 'en_US'
+LOCALE = 'en_US.UTF-8'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing

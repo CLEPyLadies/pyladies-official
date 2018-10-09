d88016b03f14e0716d102e66a92513f729cbb86b
mutterberg <Marissa@UtterbergDataDev.com>
2018-10-09 12:55:41 -0400

    Add sidebar logo and change sidebar color to dark red.

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,6 +7,25 @@ THEME = 'Flex'
 AUTHOR = 'Cleveland PyLadies'
 SITENAME = 'Cleveland PyLadies'
 SITEURL = 'https://clepyladies.github.io/pyladies-official'
+SITESUBTITLE = 'A community for PyLadies, PyNonbinaries, the PyCurious'
+SITELOGO = SITEURL + '/content/images/pylady_geek_full_dark_green.png'
+
+BROWSER_COLOR = '#810016'
+
+CC_LICENSE = {
+    'name': 'Creative Commons Attribution-ShareAlike',
+    'version': '4.0',
+    'slug': 'by-sa'
+}
+
+COPYRIGHT_YEAR = 2018
+
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'static/custom.css'},
+}
+CUSTOM_CSS = 'static/custom.css'
+
+MAIN_MENU = True
 
 PATH = 'content'
 
